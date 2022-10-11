5efbbc18afc5187ac4cc42d833f762f86eb0d291
Luisa Purificacao <luisa.purificacao@ibm.com>
2022-10-11 15:53:08 -0300

    Inner beans

---
+++ springcore/src/main/java/br/com/spring/springcore/innerbeans/Test.java
@@ -0,0 +1,18 @@
+package br.com.spring.springcore.innerbeans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"br/com/spring/springcore/innerbeans/config.xml");
+		Employee employee = (Employee) ctx.getBean("employee");
+		System.out.println(employee.getId());
+		System.out.println(employee.getAddress());
+				
+	}
+
+}
