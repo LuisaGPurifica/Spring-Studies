780526e64460db441c11fcc86482f7bb9bf58d85
Luisa Purificacao <luisa.purificacao@ibm.com>
2022-10-18 14:39:13 -0300

    Standalone Collections

---
+++ springcoreadvanced/src/main/java/br/com/spring/springcoreadvanced/standalone/collections/ProductList.java
@@ -0,0 +1,17 @@
+package br.com.spring.springcoreadvanced.standalone.collections;
+
+import java.util.List;
+
+public class ProductList {
+	
+	List<String> productNames;
+
+	public List<String> getProductNames() {
+		return productNames;
+	}
+
+	public void setProductNames(List<String> productNames) {
+		this.productNames = productNames;
+	}
+
+}
