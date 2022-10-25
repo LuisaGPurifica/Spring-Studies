81deb0c2f7a39cce091636b005433cfd7b216b86
Luisa Purificacao <luisa.purificacao@ibm.com>
2022-10-25 14:27:11 -0300

    Java configuration - class instead of.xml

---
+++ springjavaconfig/src/main/java/br/com/spring/springjavaconfig/Test.java
@@ -0,0 +1,20 @@
+package br.com.spring.springjavaconfig;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+		Service service = context.getBean(Service.class);
+		service.save();
+		System.out.println(service.hashCode());
+
+		Service service2 = context.getBean(Service.class);
+		System.out.println(service2.hashCode());
+
+		context.close();
+	}
+
+}
