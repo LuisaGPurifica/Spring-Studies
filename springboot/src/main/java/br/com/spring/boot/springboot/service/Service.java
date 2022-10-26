c87c6b98aa8a94be1e1aab5f3ed9870dcc5d8563
Luisa Purificacao <luisa.purificacao@ibm.com>
2022-10-26 09:53:04 -0300

    Spring boot created with Spring initializr

---
+++ springboot/src/main/java/br/com/spring/boot/springboot/service/Service.java
@@ -0,0 +1,23 @@
+package br.com.spring.boot.springboot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.spring.boot.springboot.dao.Dao;
+
+@Component
+public class Service {
+
+	Dao dao;
+
+	@Autowired
+	Service(Dao dao) {
+		System.out.println("Service bean created.");
+		this.dao = dao;
+	}
+
+	public void save() {
+		dao.create();
+	}
+
+}
